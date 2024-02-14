5075cde68d656de0b60ea2437da463aca1189811
Jon Herrera Hernando <jon.herrera@seidor.com>
2024-02-14 12:30:41 +0100

    Angular 17 standalone components & routes configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes'
 
-bootstrapApplication(AppComponent, appConfig)
+import { HomeComponent } from './app/components/home/home.component';
+
+bootstrapApplication(HomeComponent, {
+  providers: [
+    provideAnimations(),
+    provideRouter(routes)
+  ]
+})
   .catch((err) => console.error(err));
